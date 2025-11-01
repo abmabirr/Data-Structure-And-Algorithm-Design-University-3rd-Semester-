670964a16490ed3b2fb4602c1f6f78e7dcd17db7
Abdul Malek Abir <abdul.maleek.cse@gmail.com>
2025-11-01 10:32:14 +0600

    Create Find the sum of digits of a number using recursion.c

---
+++ Find the sum of digits of a number using recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int sum(int n) {
+    if (n == 0)
+        return 0;
+    else
+        return n + sum(n - 1);
+}
+
+int main() {
+    int n = 10;
+    printf("Sum = %d", sum(n));
+    return 0;
+}
