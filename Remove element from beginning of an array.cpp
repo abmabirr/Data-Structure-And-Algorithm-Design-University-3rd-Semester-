e25e2291190143bc8e80d21ecd3492ae417bb01a
Abdul Malek Abir <abdul.maleek.cse@gmail.com>
2025-11-20 11:23:36 +0600

    Create Remove element from beginning of an array.cpp

---
+++ Remove element from beginning of an array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100], n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter array elements:\n";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    
+    for (int i = 0; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    n--; 
+
+    cout << "Array after removing from beginning:\n";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+}
