cedaf5f20a9be58dbe1b34be89079bc668caca92
Abdul Malek Abir <abdul.maleek.cse@gmail.com>
2025-11-28 10:34:28 +0600

    Implement Quick Sort algorithm in C++

---
+++ DSA Lab final/Quick Sort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int partition(int arr[], int lb, int ub) {
+    int pivot = arr[lb];
+    int start = lb;
+    int end = ub;
+
+    while (start < end) {
+
+        while (arr[start] <= pivot && start < ub) {
+            start++;
+        }
+
+        while (arr[end] > pivot && end > lb) {
+            end--;
+        }
+
+        if (start < end) {
+            swap(arr[start], arr[end]);
+        }
+    }
+
+    swap(arr[lb], arr[end]);
+    return end;
+}
+
+void quicksort(int arr[], int lb, int ub) {
+    if (lb < ub) {
+        int loc = partition(arr, lb, ub);
+        quicksort(arr, lb, loc - 1);
+        quicksort(arr, loc + 1, ub);
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    quicksort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
