9635c3f854a57d54434ff27734150850a24565c4
Abdul Malek Abir <abdul.maleek.cse@gmail.com>
2025-11-28 19:16:01 +0600

    Create Insertion at beginning in an array.cpp

---
+++ DSA Lab final/Insertion at beginning in an array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100], n, value;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter value to insert at beginning: ";
+    cin >> value;
+
+    for(int i = n; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[0] = value;
+    n++;
+
+    cout << "Array after insertion:\n";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
