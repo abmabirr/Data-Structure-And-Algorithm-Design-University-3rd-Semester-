3d925840d62f3bcbb24da2e9e5f7d1fa2c0b7255
Abdul Malek Abir <abdul.maleek.cse@gmail.com>
2025-11-29 08:10:39 +0600

    Create BST with insert, search & delete.cpp

---
+++ BST with insert, search & delete.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+using namespace std
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+Node* insert(Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+
+bool search(Node* root, int key) {
+    if (root == NULL)
+        return false;
+
+    if (root->data == key)
+        return true;
+    else if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+
+Node* findMin(Node* root) {
+    while (root->left != NULL)
+        root = root->left;
+    return root;
+}
+
+Node* deleteNode(Node* root, int key) {
+    if (root == NULL)
+        return root;
+
+    if (key < root->data)
+        root->left = deleteNode(root->left, key);
+
+    else if (key > root->data)
+        root->right = deleteNode(root->right, key);
+
+    else { 
+        
+        if (root->left == NULL && root->right == NULL) {
+            delete root;
+            return NULL;
+        }
+        
+        else if (root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if (root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        
+        else {
+            Node* temp = findMin(root->right);
+            root->data = temp->data;
+            root->right = deleteNode(root->right, temp->data);
+        }
+    }
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+int main() {
+    Node* root = NULL;
+
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    cout << "BST (Inorder Traversal): ";
+    inorder(root);
+    cout << endl;
+
+    int key = 40;
+    if (search(root, key))
+        cout << key << " found in BST.\n";
+    else
+        cout << key << " NOT found in BST.\n";
+
+    root = deleteNode(root, 30);
+    cout << "BST after deleting 30: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}
